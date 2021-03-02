73264872db06c64ab545419b03ec8d27d45c9339
Mogakamo <mogaka.amo254@gmail.com>
2021-03-02 11:16:01 +0300

    Postcard.jsx config fileds

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -3,3 +3,31 @@ import ReactDOM from 'react-dom'
 import {} from '@material-ui/core'
 
 
+
+const PostCard = () => {
+	const classes = useStyles();
+	return (
+	  <Grid item xs={12} md={6}>
+            <Link href={post.path}>
+	      <Card className={classes.card}>
+		<div className="classes.cardetails">
+		  <CardContent>
+		    <Typography component="h2" variant="h5">
+			{post.title}
+		    </Typography>
+		    <Typography variant="subtitle1" color="textSecondary">
+		         {post.publishedAt}
+		    </Typography>
+		    <Typography variant="subtitle1" paragraph>
+			 {post.summary}
+		    </Typography>
+		    <Typography variant="subtitle1" color="primary">
+			 Continue reading....
+		    </Typography>
+		  </CardContent>
+		</div>
+	      </Card>
+	    </Link>
+	  </Grid>
+	)
+}
