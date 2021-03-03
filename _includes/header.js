810f2191800ba81ed27660227e81b7af451fced8
Mogakamo <mogaka.amo254@gmail.com>
2021-03-03 12:58:38 +0300

    Commit

--- _includes/header.js
+++ _includes/header.js
@@ -1,10 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-class Header extends React.Component {
-	return (
-		<header><p>Blog | Powered by Next.js</p></header>
-	)
+export default function Header() {
+   return <header><p>Blog | Powered by Next.js</p></header>
 }
-
-export default Header;
