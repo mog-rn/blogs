e97e2284b2a5d28a453b4e1fba840fcfceab3dd8
Mogakamo <mogaka.amo254@gmail.com>
2021-03-03 12:56:43 +0300

    Commit

---
+++ _includes/blogPost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class blogPost extends React.Component {}
+
+export default blogPost;
+
+
---
+++ _includes/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Header extends React.Component {
+	return (
+		<header><p>Blog | Powered by Next.js</p></header>
+	)
+}
+
+export default Header;
