46aaafafba17c8d89be466a00c9fc01445c971b9
Mogakamo <mogaka.amo254@gmail.com>
2021-03-03 11:51:27 +0300

    Commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const withMDX = require('@next/mdx')({
-	extension: /\.mdx?$/
-})
-
-module.exports = withMDX({
-	pageExtensions: ['js', 'jsx', 'md', 'mdx']
-});
+module.exports = {
+	target: 'serverless',
+	webpack: function (config) {
+	 config.module.rules.push({test:  /\.md$/, use: 'raw-loader'})
+	 config.module.rules.push({test:  /\.yml$/, use: 'raw-loader'})
+	 return config
+	}
+}
