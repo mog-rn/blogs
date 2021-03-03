f65e0f8d503cf37f557ac0c50d9253a966cc2569
Mogakamo <mogaka.amo254@gmail.com>
2021-03-03 12:31:25 +0300

    Commit

---
+++ _layouts/default.js
@@ -0,0 +1,18 @@
+import Head from 'next/head '
+import Header from '@includes/header'
+import Footer from '@includes/footer'
+
+
+export default function DefaultLayouts(props) {
+	return (
+	 <main>
+	    <Head>
+		<title>{props.title}</title>
+		<meta name='description' content={props.description}/>
+	    </Head>
+		<Header />
+		{props.children}
+		<Footer />
+	 </main>
+	)
+}
