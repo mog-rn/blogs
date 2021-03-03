399dc7d984f5167f307c23766f10a2aab133d047
Mogakamo <mogaka.amo254@gmail.com>
2021-03-03 12:51:45 +0300

    Commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Container, Box, Grid, Typography, CssBaseline, makeStyles } from '@material-ui/core'
- 
-export default function Blog() {
-	return <div>Welcome to Next.js blog</div>
+import DefaultLayout from '@layouts/default'
+import Link from 'next/link'
+import { getConfig, getAllPosts } from '@api'
 
+export default function Blog(props) {
+	return (
+	 <DefaultLayout title={props.title} description={props.description}>
+	 <p>List of posts:</p>
+	 <ul>
+	  {props.post.map(function (post, idx) {
+		return (
+		  <li key={idx}>
+		    <Link href={'/posts/'+post.slug}>
+			<a>{post.title}</a>
+		    </Link>
+		  </li>
+		)
+	  })}
+	 </ul>
+	 </DefaultLayout>
+	)
+}
+
+
+
+export async function getStaticProps() {
+	const config = await getConfig()
+	const allPosts = await getAllPosts()
+	return {
+	   props: {
+		posts: allPosts,
+		title: config.title,
+		description: config.description
+	   }
+
+	}
 }
