eb8c552433772b696173552e40493c3ebc047a50
Mogakamo <mogaka.amo254@gmail.com>
2021-03-03 11:43:08 +0300

    Commit

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Container, Box, Grid, Typography, CssBaseline, makeStyles } from '@material-ui/core'
-import Header from '../components/Header'
-import blogPost from '../components/blogPost'
-
-const useStyles = makeStyles({
-	root: {},  //a style rule
-	label: {} //a nested style rule
-
-})
-const Index = () => {
-	const classes = useStyles()
-	return (
-	   <React.Fragment>
-		<CssBaseline />
-		<Header />
-		<Container maxWidth="lg" className="{classes.container}">
-		    <Box my={4} display="flex" justifyContent="center">
-			<Typography variant="h4" component="h1" gutterBottom>
-				Featured Blog Posts
-			</Typography>
-		    </Box>
-			<Grid container spacing={4}>
-		           {/*{blogPosts.map(post => (
-					<PostCard key={post.title} post={post} />
-				))}*/}
-			</Grid>
-		   </Container>
-	   </React.Fragment>
-	)
+ 
+export default function Blog() {
+	return <div>Welcome to Next.js blog</div>
 
 }
-
-export default Index;
